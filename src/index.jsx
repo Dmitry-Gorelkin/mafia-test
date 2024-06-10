7cb80da01ff838956c4f06aa28174c1b1379b224
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-06-11 01:18:13 +0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="mafia/">
+  <BrowserRouter basename="mafia-test">
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
